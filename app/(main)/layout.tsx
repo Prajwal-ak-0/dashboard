505905271b69dee28cccfa91ccc4d4e92203947d
Prajwal <prajwalhassan7@gmail.com>
2023-09-29 13:11:27 +0530

    UserMenu 1.4

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,14 +1,16 @@
 import Menu from "@/components/sidebar/Menu";
 import Navbar from "@/components/navbar/Navbar";
+import { intialProfile } from "@/lib/setInitialProfile";
 
 const MainLayout = async ({
   children
 }: {
   children: React.ReactNode;
 }) => {
+  const user=await intialProfile();
     return ( 
       <Menu>
-        <Navbar/>
+        <Navbar user={user} />
       </Menu>
      );
     }
