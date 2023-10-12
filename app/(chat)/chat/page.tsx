53f67d753d1c57db083f24f926faad2a9eb5748b
Prajwal <prajwalhassan7@gmail.com>
2023-10-12 23:31:29 +0530

    Chat Started

--- app/(chat)/chat/page.tsx
+++ app/(chat)/chat/page.tsx
@@ -1,19 +1,9 @@
 import React from 'react'
-import { ChatInput } from '../components/ChatInput'
-import { db } from '@/lib/db'
 
-const ChatLayout =async () => {
-    
+const page = () => {
   return (
-    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
-      <ChatInput
-          name='Prajwal'
-          type="conversation"
-          apiUrl="/api/socket/direct-messages"
-          query={{ recipientId: 'Prajwal' }}
-        />
-    </div>
+    <div>page</div>
   )
 }
 
-export default ChatLayout
+export default page
